01813695e19a18c9e54ccdcb01c74f49ffe1be2e
1Alsaad <Mohammad.alsaad1@hotmail.com>
2024-09-15 00:44:12 +0200

    refactor: update import path for Avatar and AvatarGroup components

--- src/app/disclosure/[disclosureId]/page.tsx
+++ src/app/disclosure/[disclosureId]/page.tsx
@@ -1,4 +1,4 @@
-//src\app\disclosure\[disclosureId]\page.tsx
+//
 
 'use client';
 
@@ -27,7 +27,7 @@ import { useToast } from "@/hooks/use-toast";
 import { toast } from '@/hooks/use-toast';
 import { useDebounce } from 'use-debounce';
 import { Badge } from "@/components/ui/badge";
-import { Avatar, AvatarGroup } from "@/components/ui/avatar-group";
+import { Avatar, AvatarGroup } from "@/components/AvatarGroup";
 
 const supabaseUrl = 'https://tmmmdyykqbowfywwrwvg.supabase.co';
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
